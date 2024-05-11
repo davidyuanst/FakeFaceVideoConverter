e56884eb3d25c2909559cb5dfdcb764632fc5319
davidyuanst <davidyuanst@gmail.com>
2024-05-11 10:48:34 +0800

    Add files via upload

--- FakeFaceVideoConverter/goTest.bat
+++ FakeFaceVideoConverter/goTest.bat
@@ -1,5 +1,5 @@
-python videoConverter.py --test ./VideoFile/C141.mp4 ./VideoOutput/output1.mp4 cartoonized_pb_train_output_girl_ChineseTangwei_0h
-python videoConverter.py --test ./VideoFile/C141.mp4 ./VideoOutput/output9.mp4 cartoonized_pb_train_output_Man_Japanese3_1h
+python videoConverter.py --test ./VideoFile/test.mp4 ./VideoOutput/output1.mp4 cartoonized_pb_train_output_girl_ChineseTangwei_0h
+python videoConverter.py --test ./VideoFile/test.mp4 ./VideoOutput/output9.mp4 cartoonized_pb_train_output_Man_Japanese3_1h
 
 
 
