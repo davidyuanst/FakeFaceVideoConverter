d46ec277287e94c41288dced24d89f8f4dc39ac4
davidyuanst <davidyuanst@gmail.com>
2024-05-14 15:52:27 +0800

    Delete FakeFaceVideoConverter directory

--- FakeFaceVideoConverter/goTest.bat
+++
@@ -1,6 +0,0 @@
-python videoConverter.py --test ./VideoFile/9_04_09_19.mp4 ./VideoOutput/output1.mp4 ./model_pb/pb_train_output_girl_ChineseTangwei_0h
-python videoConverter.py ./VideoFile/C141.mp4 ./VideoOutput/output12.mp4 ./model_pb/pb_train_output_Man_Japanese3_1h
-
-
-
-
